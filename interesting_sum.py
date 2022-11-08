e128b5cf3aad230294c02054c5e2b90d80bd8fb1
amfurstain <117762062+amfurstain@users.noreply.github.com>
2022-11-08 19:31:15 +0200

    Update interesting_sum.py

--- interesting_sum.py
+++ interesting_sum.py
@@ -1,6 +1,11 @@
-n = str(int(input('Введіть число: ')))
+n = int(input('Введіть число: '))
 
-min_n = ''.join(sorted(n))
-max_n = ''.join(sorted(n, reverse=True))
+if n <= 999 and n >= 100:
+    n = str(n)
 
-print(int(min_n) + int(max_n))
+    min_n = ''.join(sorted(n))
+    max_n = ''.join(sorted(n, reverse=True))
+    
+    print(int(min_n) + int(max_n))
+else:
+    print('Введіть тризначне число')
